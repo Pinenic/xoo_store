a2e352e93e7ebbf32184e2c69d47ab203f92f785
EphiusJB <cloudjoseph797@gmail.com>
2025-08-15 13:24:13 +0200

    login/signup flow cleanup

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import UserAuth from '../components/UserAuth'
-import Auth from '../components/Auth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Auth />
+      <UserAuth />
     </>
   )
 }
