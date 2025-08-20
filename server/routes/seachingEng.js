7842f843c26dab7a7dfa2bcd2051b5f2e41ad276
4in-com <russelmuyembe9@gmail.com>
2025-08-20 04:03:13 -0700

    i fixed file upload to upload file with file contentType

--- server/routes/seachingEng.js
+++ server/routes/seachingEng.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const supabase = require("../index");
 
 //this route seach for a products with the matching content,
-// seach iterm is passed through the body
+// seach iterm is passed through the query parameter `q` in the URL
+// you can also pass a limit to the number of products returned using the query parameter `limit`
+// the limit is set to the number of matching products if not specified
 
-router.post("/", async (req, res) => {
-const seaching=await req.body.seach;
+router.get("/", async (req, res) => {
+const seaching=await req.query.q;
+const countLimit = (await supabase.from("products").select("*").like("name",`%${seaching}%`)).data.length;
+const limit = req.query.limit || countLimit; // Default limit to the number of matching products
   try {
-    const { data } = await supabase.from("products").select("*").like("name",`%${seaching}%`);
+    const { data } = await supabase.from("products").select("*").like("name",`%${seaching}%`).limit(limit);
     res.status(200).send(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
