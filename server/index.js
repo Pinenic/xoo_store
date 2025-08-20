b7e783381a457fa05c06b4aba1c22788a08bed39
4in-com <russelmuyembe9@gmail.com>
2025-08-20 05:49:44 -0700

    auto delete to images of store if store is deleted through the data base by the deleveloper

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express     = require("express");
-const app         = express();
-const bodyParser  = require('body-parser');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
@@ -12,32 +12,21 @@ module.exports = supabase;
 
 //routes
 const productRouter = require("./routes/products");
-const userRouter    = require("./routes/users");
-const storeRoute    = require("./routes/stores");
-const seachRoute    = require("./routes/seachingEng");
-const orderRoute    = require('./routes/order');
+const userRouter = require("./routes/users");
+const storeRoute = require("./routes/stores");
+const seachRoute = require("./routes/seachingEng");
+const orderRoute = require("./routes/order");
 
 // midlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 const api = process.env.API_ROOT;
 app.use(`/${api}/products`, productRouter);
 app.use(`/${api}/users`, userRouter);
-app.use(`/${api}/stores`, storeRoute );
-app.use(`/${api}/seach`,seachRoute);
-app.use(`/${api}/orders`,orderRoute);
+app.use(`/${api}/stores`, storeRoute);
+app.use(`/${api}/seach`, seachRoute);
+app.use(`/${api}/orders`, orderRoute);
 
-app.get("/", async(req, res)=>{
-    try {
-       const data= await supabase.storage
-    .from("imageUpload").list('public');
-// const list=data.map((files)=> `files.name`);
-
-    res.status(200).send(data.data);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-})
 
 const port = process.env.PORT || 4000;
 
